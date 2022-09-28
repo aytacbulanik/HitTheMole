81d3af371255065c32f7583eb312e6ef6f98f22a
Aytac Bulanık <aytacbulanik@hotmail.com>
2022-09-28 22:53:37 +0300

    Initial Commit

---
+++ HitTheMole/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HitTheMole
+//
+//  Created by AYTAÇ BULANIK on 28.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
