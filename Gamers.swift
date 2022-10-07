88614009a039da1e7b3fd1dceb3b0826cbb4c9ff
Aytaç Bulanık <aytacbulanik@hotmail.com>
2022-10-08 00:48:30 +0300

    gamers realm file added

---
+++ Gamers.swift
@@ -0,0 +1,20 @@
+//
+//  Gamers.swift
+//  HitTheMole
+//
+//  Created by AYTAÇ BULANIK on 8.10.2022.
+//
+
+import Foundation
+import RealmSwift
+
+class Gamers : Object {
+    @Persisted var userName : String = ""
+    @Persisted var createDate : Date = Date.now
+    
+    convenience init(userName : String , createDate : Date) {
+        self.init()
+        self.userName = userName
+        self.createDate = createDate
+    }
+}
